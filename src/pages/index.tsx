5216d88c1ccd0eb254939f1de2ed5d19d432a5a3
Theerth1 <theerth.srinivasan@gmail.com>
2026-04-14 21:02:44 -0400

    created TitleBar.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import CornerDecoration from "@/components/CornerDecoration";
+
 export default function Home() {
   return <div><CornerDecoration/></div>;
 }
